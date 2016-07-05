f4560bf01bf60c87919bfbd8d2f46e006dfc5198
DellSoftware-SimonDu <simon.du@software.dell.com>
2016-07-05 09:28:36 +0800

    4
    4

--- Main.Tests.ps1
+++ Main.Tests.ps1
@@ -1,6 +1,6 @@
 ﻿
-.\UploadSampleData.ps1
-. .\CheckResult.ps1
+#.\UploadSampleData.ps1
+#. .\CheckResult.ps1
 
 Describe -Tags "HealthCheck" "HealthCheck Results" {
  
@@ -16,3 +16,50 @@ Describe -Tags "HealthCheck" "HealthCheck Results" {
    }
 }
 
+function CheckResult
+{
+$CompareJson = @'
+  {
+  "TotalScoreIncludingIgnoredChecks": [
+    77.443280977312384,
+    75.443280977312384
+  ],
+  "TotalScoreExcludingIgnoredChecks": [
+    77.443280977312384,
+    75.443280977312384
+  ],
+  "TopIssues": {
+    "_t": "a",
+    "4": [
+      "memory_physical_memory_pressure"
+    ]
+  },
+  "StaticHealthChecks": {
+    "_t": "a",
+    "0": {
+      "HealthCheckScore": [
+        100.0,
+        95.0
+      ]
+    }
+  },
+  "PhysicalMemoryPressure": {
+    "TrendAnalysis": {
+      "TrendExists": [
+        false,
+        true
+      ],
+      "TrendIsIncreasing": [
+        false,
+        true
+      ]
+    }
+  }
+}
+'@
+
+if ($CompareJson -eq $null)
+{
+    return $true
+}
+}  
