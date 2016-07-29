e3c42d65e7118b8835c1565b9a9026c7476256dd
DellSoftware-SimonDu <simon.du@software.dell.com>
2016-07-29 16:36:42 +0800

    1
    1

---
+++ helloworld/Program.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+
+namespace helloworld
+{
+    [TestFixture]
+    public class Program
+    {
+        //  static void Main(string[] args)
+        //  {
+        //      System.Console.WriteLine("Hello, World!");
+        //       Console.ReadKey();
+        //   }
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello, World!");
+                 
+        }
+    }
+}
